2dc25c80c65097623ad8e658de8bc6f07722cf18
sylrdev <sylrdev@gmail.com>
2026-07-16 15:44:30 +0200

    initial commit

---
+++ lua/options.lua
@@ -0,0 +1,20 @@
+local opt = vim.opt
+
+opt.number = true
+opt.relativenumber = true
+opt.signcolumn = "yes"
+opt.cursorline = true
+opt.scrolloff = 999
+opt.tabstop = 4
+opt.shiftwidth = 4
+opt.winborder = "rounded"
+opt.clipboard = "unnamedplus"
+opt.exrc = true
+opt.secure = true
+
+vim.diagnostic.config {
+	float = {
+		border = "rounded",
+	},
+	virtual_text = true,
+}
