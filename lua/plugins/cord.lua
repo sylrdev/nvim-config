a8c09eec68e6465186071f7f67d127e72e045a2b
sylrdev <sylrdev@gmail.com>
2026-07-19 11:29:22 +0200

    feat: make cord tooltip auto updated

--- lua/plugins/cord.lua
+++ lua/plugins/cord.lua
@@ -1,6 +1,27 @@
+local function getTooltip()
+	-- getting system name
+	local systemName = "Unknown"
+
+	if vim.fn.has("win32") == 1 then
+		systemName = "Windows"
+	elseif vim.fn.has("linux") == 1 then
+		systemName = "Linux"
+	elseif vim.fn.has("mac") == 1 then
+		systemName = "MacOS"
+	end
+
+	return string.format(
+		"Nvim %i.%i.%i on %s",
+		vim.version().major,
+		vim.version().minor,
+		vim.version().patch,
+		systemName
+	)
+end
+
 require("cord").setup {
 	editor = {
-		tooltip = "Nvim 0.12.4 on Windows",
+		tooltip = getTooltip(),
 	},
 	display = {
 		theme = "catppuccin",
